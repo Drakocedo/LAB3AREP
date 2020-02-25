a7914703f5a34ec2356569ecc172239fa8fcd34a
unknown <david.caycedo@labinfo.is.escuelaing.edu.co>
2020-02-25 10:54:26 -0500

    avance

--- clientServer/src/main/java/edu/escuelaing/arep/app/HttpServer.java
+++ clientServer/src/main/java/edu/escuelaing/arep/app/HttpServer.java
@@ -9,14 +9,16 @@ package edu.escuelaing.arep.app;
  *
  * @author david
  */
+import java.awt.image.BufferedImage;
 import java.net.*;
 import java.io.*;
+import javax.imageio.ImageIO;
 
 public class HttpServer {
   public static void main(String[] args) throws IOException {
    ServerSocket serverSocket = null;
    int puerto = getPort();
-   String path="";
+   
 
    while (true){
         try { 
@@ -38,61 +40,116 @@ public class HttpServer {
         BufferedReader in = new BufferedReader(
                               new InputStreamReader(clientSocket.getInputStream()));
         String inputLine, outputLine;
-        while ((inputLine = in.readLine()) != null) {
-           System.out.println("Recibí: " + inputLine);
-           if (!in.ready()) {break; }
-           if (inputLine.contains("GET")){
-               //System.out.println("Vamos bien: " + inputLine);
-               //System.out.println(path);
-               
-            
-            path = inputLine.substring(inputLine.indexOf("/"),inputLine.lastIndexOf(" "));
-            
-        
-           }
-        }
-        
-        if (path.contains("PNG") || path.contains("png")){
-            
-        }    
-                                  
-        
-        outputLine = "HTTP/1.1 200 OK\r\n"
-             + "Content-Type: text/html\r\n"
-              + "\r\n"
-              + "<!DOCTYPE html>\n"
-              + "<html>\n"
-              + "<head>\n"
-              + "<meta charset=\"UTF-8\">\n"
-              + "<title>Title of the document</title>\n"
-              + "</head>\n"
-              + "<body>\n"
-              + "<h1>Hola samuel</h1>\n"
-              + "</body>\n"
-              + "</html>\n" + inputLine;
-         out.println(outputLine);
-         out.close(); 
-         in.close(); 
-         clientSocket.close(); 
-         serverSocket.close();
-   }     
-  }
-  
+        while ((inputLine = in.readLine()) != null) {   
+                //System.out.println("Recibí: " + inputLine);
 
-   static String getPath(String archivo){
-        String path = "src/main/resources/";
+                if (inputLine.contains("GET")){
+                    System.out.println("Vamos bien: " + inputLine);
+                    //System.out.println(path);
+
+                 String path="";
+                 path = inputLine.substring(inputLine.indexOf("/"),inputLine.lastIndexOf(" "));
+                 path = getPath(path);
+
+                 File archivo = new File(path);
+                if (archivo.exists()) {
+                if (path.contains("PNG") || path.contains("png")){
+                     getImagen(path, clientSocket.getOutputStream(),out);            
+                }    
+                else if (path.contains(".html") || path.contains(".js")){
+                     getArchivo(path, clientSocket.getOutputStream()); 
+                }}
+                else {
+                   outputLine = "HTTP/1.1 200 OK\r\n"
+                   + "Content-Type: text/html\r\n"
+                   + "\r\n"
+                   + "<!DOCTYPE html>\n"
+                   + "<html>\n"
+                   + "<head>\n"
+                   + "<meta charset=\"UTF-8\">\n"
+                   + "<title>Title of the document</title>\n"
+                   + "</head>\n"
+                   + "<body>\n"
+                   + "<h1>No existe el archivo seleccionado</h1>\n"
+                   + "</body>\n"
+                   + "</html>\n" + inputLine;
+                  out.println(outputLine); 
+                }
+             }       
+             if (!in.ready()) {break; }                      
+            } 
+              out.close(); 
+              in.close(); 
+              clientSocket.close(); 
+              serverSocket.close();
+            }
+        
+     
+  }
+
+   private static String getPath(String archivo){
+        String path = "/src/main/resources";
         return path+archivo;
    }
    
+
    
-   
+   public static void getImagen(String type, OutputStream clienteOutput, PrintWriter out) throws IOException {
+        try {
+            BufferedImage image = ImageIO.read(new File(System.getProperty("user.dir") + type));
+            ByteArrayOutputStream ArrBytes = new ByteArrayOutputStream();
+            DataOutputStream writeImg = new DataOutputStream(clienteOutput);
+            String img = "HTTP /1.1 404 NOT FOUND \r\n"
+                    + "Content-Type: text/html; charset=\"UTF-8\" \r\n"
+                    + "\r\n";
+            ImageIO.write(image, "PNG", ArrBytes);
+            writeImg.writeBytes("HTTP/1.1 200 OK \r\n");
+            writeImg.writeBytes("Content-Type: image/png \r\n");
+            writeImg.writeBytes("\r\n");
+            writeImg.write(ArrBytes.toByteArray());
+            System.out.println(System.getProperty("user.dir") + type);
+        } catch (IOException e) {
+            System.out.println("r" + e.getMessage());
+        }
+    }
+
+    
+    private static void getArchivo(String route, OutputStream outputStream) throws IOException {
+        
+        try {
+            String text = "";
+            String temp;
+            BufferedReader t = new BufferedReader(new FileReader(System.getProperty("user.dir") + route));
+            while ((temp = t.readLine()) != null) {
+             
+                text = text + temp;
+            }
+            if (route.contains(".js")){
+                outputStream.write(("HTTP/1.1 201 FOUND  \r\n"
+                        + "Content-Type: application/json; charset=\"UTF-8\" \r\n"
+                        + "\r\n"
+                        + text).getBytes());
+                }
+            else if (route.contains(".html")){
+                outputStream.write(("HTTP/1.1 201 Found  \r\n"
+                    + "Content-Type: text/html; charset=\"utf-8\" \r\n"
+                    + "\r\n"
+                    + text).getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
    
   
    static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 36000;
+        return 57000;
     }
   
 }
